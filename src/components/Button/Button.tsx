7633a6fd6f543aa702e5e0379361f52946548324
ritaaaaa-OFFICIAL <ritaaaaa@simpleinfo.cc>
2022-04-22 16:01:19 +0800

    add styled-component as peerdependencies

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
 export interface ButtonProps {
   label: string;
+  primary: boolean;
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+const Button = ({ label, ...rest }: ButtonProps) => {
+  return <StyledButton {...rest}>{label}</StyledButton>;
 };
+
+const StyledButton = styled.button`
+  line-height: 40px;
+  border: 2px solid red;
+  padding: 8px 16px;
+`;
 
 export default Button;
